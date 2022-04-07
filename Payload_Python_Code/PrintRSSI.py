d3d3e683c891468b9314af1ccf687bf6cd3c5296
tewicklund <91501788+tewicklund@users.noreply.github.com>
2022-04-06 20:05:45 -0700

    Create PrintRSSI.py
    Only Prints the RSSI of the latest received packet

---
+++ Payload_Python_Code/PrintRSSI.py
@@ -0,0 +1,35 @@
+#https://learn.adafruit.com/lora-and-lorawan-radio-for-raspberry-pi/raspberry-pi-wiring
+#sudo pip3 install adafruit-circuitpython-rfm9x
+
+import time
+import RPi.GPIO as GPIO
+import board
+import busio
+from digitalio import DigitalInOut, Direction, Pull
+import adafruit_rfm9x
+
+# Configure RFM9x LoRa Radio
+
+CS = DigitalInOut(board.CE1)
+RESET = DigitalInOut(board.D25)
+spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
+
+
+try:
+    rfm9x = adafruit_rfm9x.RFM9x(spi, CS, RESET, 915.0)
+    print('RFM9x: Detected')
+except RuntimeError as error:
+    # Thrown on version mismatch
+
+    print('RFM9x Error: ', error)
+while True:
+    packet=None
+    packet=rfm9x.receive()
+    if packet is None:
+        print("waiting for packet")
+    else:
+        prev_packet=packet
+        packet_text=str(prev_packet, "utf-8")
+        print(str(rfm9x.last_rssi))
+    time.sleep(0.1)
+
