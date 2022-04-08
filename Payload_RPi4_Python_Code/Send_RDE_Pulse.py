c5753007f043c9923ddeb7202b0c463bedbc2fb8
tewicklund <91501788+tewicklund@users.noreply.github.com>
2022-04-08 13:51:30 -0700

    Create Send_RDE_Pulse.py

---
+++ Payload_RPi4_Python_Code/Send_RDE_Pulse.py
@@ -0,0 +1,33 @@
+#General setup:
+import time
+import RPi.GPIO as GPIO
+import board
+import busio
+from digitalio import DigitalInOut, Direction, Pull
+import adafruit_rfm9x
+
+# Configure RFM9x LoRa Radio
+CS = DigitalInOut(board.CE1)
+RESET = DigitalInOut(board.D25)
+spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
+
+#Configure the GPIO:
+GPIO.setmode(GPIO.BCM)
+
+#Check connection to Radio:
+try:
+    rfm9x = adafruit_rfm9x.RFM9x(spi, CS, RESET, 915.0)
+    print('RFM9x: Detected')
+    #set transmission power to max (5 to 23)
+    rfm9x.tx_power=23
+    
+except RuntimeError as error:
+    # Thrown on version mismatch
+    print('RFM9x Error: ', error)
+ 
+#Rapidly send the string "RDE"
+while True:
+
+    message=bytes("RDE","utf-8")
+    rfm9x.send(message)
+    time.sleep(0.01)
