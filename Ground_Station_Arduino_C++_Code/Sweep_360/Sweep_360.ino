dc16e184b712a9e156783a2914654b1f2dfe75c9
tewicklund <91501788+tewicklund@users.noreply.github.com>
2022-04-09 18:53:24 -0700

    Update Sweep_360.ino
    Removed reference to "wire" (i2c junk), corrected comments, and made sure that the transmitted string over UART looks like "UUU####;;;"

--- Ground_Station_Arduino_C++_Code/Sweep_360/Sweep_360.ino
+++ Ground_Station_Arduino_C++_Code/Sweep_360/Sweep_360.ino
@@ -2,7 +2,7 @@
 //tutorial for i2c data transfer: https://www.arduino.cc/en/Tutorial/LibraryExamples/MasterWriter
 
 #include <Servo.h>
-#include <Wire.h>
+
 
 Servo myservo;  // create servo object to control a servo
 // twelve servo objects can be created on most boards
@@ -10,35 +10,40 @@ Servo myservo;  // create servo object to control a servo
 
 void setup() {
   myservo.attach(9);  // attaches the servo on pin 9 to the servo object
-  Wire.begin(); // join i2c bus (address optional for master)
   Serial1.begin(9600);
-  
 }
 
 int pos = 0;    // variable to store the servo position
 
 void loop() {
-  for (pos = 600; pos <=2400 ; pos += 1) { // goes from 0 degrees to 360 degrees
+  for (pos = 600; pos <=2400 ; pos += 1) { 
     // in steps of 1 degree
     myservo.writeMicroseconds(pos);              // tell servo to go to position in variable 'pos'
-    Serial1.println("U"+String(pos)+";");
     
-    //put code here to send the pos over i2c
-    Wire.beginTransmission(4); // transmit to device #4
-    Wire.write(pos);           //send int pos
-    Wire.endTransmission();    // stop transmitting
+    //write a string formatted as "UUU####;;;" where the # are chars of numbers, example "UUU2038;;;"
+    if (pos<999)
+    {
+      Serial1.println("UUU0"+String(pos)+";;;");
+    }
+    else
+    {
+      Serial1.println("UUU"+String(pos)+";;;");
+    }
     
     delay(30);                       // waits 15 ms for the servo to reach the position
   }
-  for (pos = 2400; pos >= 600; pos -= 1) { // goes from 360 degrees to 0 degrees
+  for (pos = 2400; pos >= 600; pos -= 1) { 
     myservo.writeMicroseconds(pos);              // tell servo to go to position in variable 'pos'
-    Serial1.println("UUU"+String(pos)+";;;");
     
-    //put code here to send the pos over i2c
-    Wire.beginTransmission(4); // transmit to device #4
-    Wire.write(pos);           //send int pos
-    Wire.endTransmission();    // stop transmitting
-    
-    delay(30);                       // waits 15 ms for the servo to reach the position
+    //write a string formatted as "UUU####;;;" where the # are chars of numbers, example "UUU2038;;;"
+    if (pos<999)
+    {
+      Serial1.println("UUU0"+String(pos)+";;;");
+    }
+    else
+    {
+      Serial1.println("UUU"+String(pos)+";;;");
+    }
+    delay(30);                       // waits 30 ms for the servo to reach the position
   }
 }
