4c04a5862f12451f9f1f70b81b393137cbb80085
rocketpi4 <rocketpi4@eclipse.davis>
2022-04-13 18:34:46 -0700

    add dependency for numpy to install script

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 sudo apt update
 sudo apt upgrade
-sudo apt install -y git python3-pip libglib2.0-dev libgirepository1.0-dev libcairo2-dev
+sudo apt install -y git python3-pip libglib2.0-dev libgirepository1.0-dev libcairo2-dev libatlas-base-dev
 
 sudo pip3 install -r requirements.txt
