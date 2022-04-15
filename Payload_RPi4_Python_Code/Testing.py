52c158bb086af987ea824696ff6f31287d05964d
ECLIPSEjacob <103392775+ECLIPSEjacob@users.noreply.github.com>
2022-04-15 00:11:33 -0700

    Update Testing.py

--- Payload_RPi4_Python_Code/Testing.py
+++ Payload_RPi4_Python_Code/Testing.py
@@ -21,7 +21,7 @@ GPIO.setmode(GPIO.BCM)
 
 #Check connection to Radio:
 try:
-    rfm9x = adafruit_rfm9x.RFM9x(spi, CS, RESET, 915.0)
+    rfm9x = adafruit_rfm9x.RFM9x(spi, CS, RESET, 910.0)
     print('RFM9x: Detected')
     #set transmission power to max (5 to 23)
     rfm9x.tx_power=23
