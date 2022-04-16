f577d0d341d55865155a2e0f326fe8b66ae7e066
ECLIPSEjacob <103392775+ECLIPSEjacob@users.noreply.github.com>
2022-04-16 14:00:31 -0700

    Update FlightCode.py

--- Payload_RPi4_Python_Code/FlightCode.py
+++ Payload_RPi4_Python_Code/FlightCode.py
@@ -44,7 +44,7 @@ def findstart():
         else:
             print("Found Start Signal")
             message=bytes("START","utf-8")
-            for i in range(30):
+            for i in range(100):
                 rfm9x.send(message)
                 time.sleep(0.01)
                 print("Sending Confirmation")
